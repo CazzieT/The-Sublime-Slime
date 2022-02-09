0372f7b4e8271a1dae9f5d172f63c98b2dd40bb6
CazzieT <99173864+CazzieT@users.noreply.github.com>
2022-02-09 11:34:36 +0100

    Bottons on menus

--- The Sublime Slime/Assets/Scripts/MenuScript.cs
+++ The Sublime Slime/Assets/Scripts/MenuScript.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    public void GotoScene(string sceneName)
+    public void startFunktion(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
